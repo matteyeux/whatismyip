08909461815e51f88ac9977b38dfcce8a5a264f1
Mathieu <mathieu.hautebas2@gmail.com>
2015-03-25 16:01:12 +0100

    Create ip.pl

---
+++ ip.pl
@@ -0,0 +1,15 @@
+#!/usr/bin/perl
+#Developed by @matteyeux
+
+use LWP::Simple;
+
+$content = get("http://icanhazip.com");
+
+	
+print "$content\n\n";
+
+<>;
+
+print "\nFollow me on Twitter : matteyeux\n";
+
+<>;
